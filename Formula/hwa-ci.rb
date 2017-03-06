493d35e8dea1a97161927a2fea95998adf1adfb7
John Ky <newhoggy@gmail.com>
2017-03-07 08:15:58 +1100

    New formula hwa-ci

---
+++ Formula/hwa-ci.rb
@@ -0,0 +1,22 @@
+require "language/haskell"
+
+class HwaCi < Formula
+  include Language::Haskell::Cabal
+
+  desc "CI configuration tool"
+  homepage "https://github.com/haskell-works/hwa-ci"
+  url "https://github.com/haskell-works/hwa-ci/archive/v0.1.0.0.tar.gz"
+  sha256 "f9ac84bcdcdad559e44b7fd301bb4437136c2be23f304cd9742b93e623659af6"
+  head "https://github.com/Gabriel439/Haskell-Dhall-JSON-Library"
+
+  depends_on "ghc" => :build
+  depends_on "cabal-install" => :build
+
+  def install
+    install_cabal_package
+  end
+
+  test do
+    assert_match "hwa-ci 0.1.0.0", pipe_output("#{bin}/hwa-ci version", "", 0)
+  end
+end
