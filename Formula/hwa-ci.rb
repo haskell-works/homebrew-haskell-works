8b0669c6f29c7ec8f9793dc33e7d28e35325ace9
John Ky <newhoggy@gmail.com>
2017-03-14 22:16:27 +1100

    New version of hwa-ci

--- Formula/hwa-ci.rb
+++ Formula/hwa-ci.rb
@@ -5,7 +5,7 @@ class HwaCi < Formula
 
   desc "CI configuration tool"
   homepage "https://github.com/haskell-works/hwa-ci"
-  url "https://github.com/haskell-works/hwa-ci/archive/v0.1.0.0.tar.gz"
+  url "https://github.com/haskell-works/hwa-ci/archive/v0.1.0.1.tar.gz"
   sha256 "f9ac84bcdcdad559e44b7fd301bb4437136c2be23f304cd9742b93e623659af6"
   head "https://github.com/Gabriel439/Haskell-Dhall-JSON-Library"
 
@@ -14,12 +14,6 @@ class HwaCi < Formula
 
   def install
     install_cabal_package
-  end
-
-  bottle do
-    root_url "https://github.com/haskell-works/homebrew-haskell-works/releases/download/hwa-ci-0.1.0.0"
-    cellar :any_skip_relocation
-    sha256 "db6ec38aced981628c1d0ba001a5405826236374c591c66f74d58baaa4e42dc8" => :sierra
   end
 
   test do
