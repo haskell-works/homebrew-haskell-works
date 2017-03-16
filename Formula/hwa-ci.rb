4ade206f66d16c988fbaa7269dcf90a526678991
John Ky <newhoggy@gmail.com>
2017-03-16 23:31:27 +1100

    hwa-ci-0.1.0.2

--- Formula/hwa-ci.rb
+++ Formula/hwa-ci.rb
@@ -5,8 +5,8 @@ class HwaCi < Formula
 
   desc "CI configuration tool"
   homepage "https://github.com/haskell-works/hwa-ci"
-  url "https://github.com/haskell-works/hwa-ci/archive/v0.1.0.1.tar.gz"
-  sha256 "70d9b28c073d59f3574da2cb0735d3556ec7605def38b3486dbc5981771781a0"
+  url "https://github.com/haskell-works/hwa-ci/archive/v0.1.0.2.tar.gz"
+  sha256 "6cd2c65382df5eb91fe7a18d635aae2039becd17d315604633588a06ea86f462"
   head "https://github.com/haskell-works/hwa-ci"
 
   depends_on "ghc" => :build
@@ -20,9 +20,9 @@ class HwaCi < Formula
   end
 
   bottle do
-    root_url "https://github.com/haskell-works/homebrew-haskell-works/releases/download/hwa-ci-0.1.0.1"
+    root_url "https://github.com/haskell-works/homebrew-haskell-works/releases/download/hwa-ci-0.1.0.2"
     cellar :any_skip_relocation
-    sha256 "b611873018d0cd9c291cc7660391d699c01037fa5d9068ae166e8a7606595568" => :sierra
+    sha256 "c44ebccad5a135dd7cad7a529b0ff06bc7772e1e591aaff831aca79923ec9747" => :sierra
   end
 
   test do
