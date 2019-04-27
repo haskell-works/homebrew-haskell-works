462a063633ce4c4cd3ff9a75525dca7fd0d599cb
John Ky <newhoggy@gmail.com>
2019-04-27 11:46:07 +1000

    Upgrade to cabal-cache-1.0.0.0

--- Formula/cabal-cache.rb
+++ Formula/cabal-cache.rb
@@ -5,8 +5,8 @@ class CabalCache < Formula
 
   desc "CI configuration tool"
   homepage "https://github.com/haskell-works/cabal-cache"
-  url "https://github.com/haskell-works/cabal-cache/archive/v0.2.0.2.tar.gz"
-  sha256 "e7706709e90b6f184fa9ceee2b8a8643b145c15f3c1bde9ad2a1362d57797918"
+  url "https://github.com/haskell-works/cabal-cache/archive/v1.0.0.0.tar.gz"
+  sha256 "ec1cf96935e5d69b9554270b11f13261ef7b0e890d32d9be8b6cb53af31f93a3"
   head "https://github.com/haskell-works/cabal-cache"
 
   depends_on "ghc" => :build
@@ -20,10 +20,10 @@ class CabalCache < Formula
   end
 
   bottle do
-    root_url "https://github.com/haskell-works/homebrew-haskell-works/releases/download/cabal-cache-0.2.0.2"
+    root_url "https://github.com/haskell-works/homebrew-haskell-works/releases/download/cabal-cache-1.0.0.0"
     cellar :any_skip_relocation
     rebuild 1
-    sha256 "93e60f6adb2b981d0a1e148f6a7793438ce3e2cafb57f2a77abf142233e3965a" => :mojave
+    sha256 "2962040c149cdbec23e492245951cdb6c3d5c113aa5a648ec5f502901f32e328" => :mojave
   end
 
   test do
