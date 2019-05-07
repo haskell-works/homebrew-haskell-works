f55bc6b4c9bbfbce935ff74ae7f3b90bb7a5c4c3
John Ky <newhoggy@gmail.com>
2019-05-07 23:44:11 +1000

    New version cabal-cache-1.0.0.3

--- Formula/cabal-cache.rb
+++ Formula/cabal-cache.rb
@@ -5,8 +5,8 @@ class CabalCache < Formula
 
   desc "CI configuration tool"
   homepage "https://github.com/haskell-works/cabal-cache"
-  url "https://github.com/haskell-works/cabal-cache/archive/v1.0.0.1.tar.gz"
-  sha256 "ec1cf96935e5d69b9554270b11f13261ef7b0e890d32d9be8b6cb53af31f93a3"
+  url "https://github.com/haskell-works/cabal-cache/archive/v1.0.0.3.tar.gz"
+  sha256 "ac4b4bec3df10860ae1cc40dc637f07155f07229b0802274f6e4ecacd4e36378"
   head "https://github.com/haskell-works/cabal-cache"
 
   depends_on "ghc" => :build
@@ -20,18 +20,18 @@ class CabalCache < Formula
   end
 
   bottle do
-    root_url "https://github.com/haskell-works/homebrew-haskell-works/releases/download/cabal-cache-1.0.0.1"
+    root_url "https://github.com/haskell-works/homebrew-haskell-works/releases/download/cabal-cache-1.0.0.3"
     cellar :any_skip_relocation
     rebuild 1
-    sha256 "0ee131962af50b97bc90cb51500de18bf0769416c56c112948f9effd8d15911a" => :mojave
+    sha256 "887075afac56746008caecf19c61e5891afde37a963d2ea5fac538429a65afb6" => :mojave
   end
 
-  bottle do
-    root_url "https://github.com/haskell-works/homebrew-haskell-works/releases/download/cabal-cache-1.0.0.1"
-    cellar :any_skip_relocation
-    rebuild 1
-    sha256 "0ee131962af50b97bc90cb51500de18bf0769416c56c112948f9effd8d15911a" => :high_sierra
-  end
+  # bottle do
+  #   root_url "https://github.com/haskell-works/homebrew-haskell-works/releases/download/cabal-cache-1.0.0.1"
+  #   cellar :any_skip_relocation
+  #   rebuild 1
+  #   sha256 "0ee131962af50b97bc90cb51500de18bf0769416c56c112948f9effd8d15911a" => :high_sierra
+  # end
 
   test do
     assert_match "cabal-cache 0.2.0.2", pipe_output("#{bin}/cabal-cache version", "", 0)
