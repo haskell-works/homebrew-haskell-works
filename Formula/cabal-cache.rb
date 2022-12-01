66c310befd4fcd849ea61e859fe3a41ee410efa0
John Ky <newhoggy@gmail.com>
2022-12-01 20:43:22 +1100

    New cabal-cache version 1.0.5.1

--- Formula/cabal-cache.rb
+++ Formula/cabal-cache.rb
@@ -5,7 +5,7 @@ class CabalCache < Formula
 
   desc "CI configuration tool"
   homepage "https://github.com/haskell-works/cabal-cache"
-  url "https://github.com/haskell-works/cabal-cache/archive/v1.0.0.12.tar.gz"
+  url "https://github.com/haskell-works/cabal-cache/archive/v1.0.5.1.tar.gz"
   sha256 "6a8bb1347a67e274e2b1f3eea8171197e0cffccd3c12a17ecdda308edae77c32"
   head "https://github.com/haskell-works/cabal-cache"
 
@@ -20,7 +20,7 @@ class CabalCache < Formula
   end
 
   bottle do
-    root_url "https://github.com/haskell-works/homebrew-haskell-works/releases/download/cabal-cache-1.0.0.12"
+    root_url "https://github.com/haskell-works/homebrew-haskell-works/releases/download/cabal-cache-1.0.5.1"
     cellar :any_skip_relocation
     rebuild 1
     sha256 "384c306f4a47b7ae7db03c6dddb036bdb069c4d4e78d540d821c018b642069dc" => :mojave
